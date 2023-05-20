67bf3fdade4507b53b7347d4ac8d6f6d5306ea4c
Emerson <emerson.day@gmail.com>
2023-05-19 20:27:05 -0700

    added episodes fetch

--- src/pages/episodes/[number].tsx
+++ src/pages/episodes/[number].tsx
@@ -1,3 +1,13 @@
+import { useRouter } from 'next/router'
+import { useQuery } from '@tanstack/react-query'
+import { fetcher } from '../../utils/fetch'
 export default function Episode() {
+    const router = useRouter()
+    const id = router.query.id
+    const { status, data } = useQuery({
+        queryKey: ['episode', id],
+        queryFn: () =>
+            fetcher(`https://rickandmortyapi.com/api/character/${id}`),
+    })
     return <div className="min-h-screen">Episode</div>
 }
