c7a509d96e8401e1f32b88e652925e9e01be1b7f
Emerson <emerson.day@gmail.com>
2023-05-31 14:58:06 -0700

    episodes by season

--- src/pages/episodes.tsx
+++ src/pages/episodes.tsx
@@ -1,4 +1,26 @@
 function Episodes() {
-    return <div>Episodes</div>
+    return (
+        <div>
+            <h1 className="text-3xl">episodes</h1>
+            <div>
+                <h2>season 1</h2>
+            </div>
+            <div>
+                <h2>season 2</h2>
+            </div>
+            <div>
+                <h2>season 3</h2>
+            </div>
+            <div>
+                <h2>season 4</h2>
+            </div>
+            <div>
+                <h2>season 5</h2>
+            </div>
+            <div>
+                <h2>season 6</h2>
+            </div>
+        </div>
+    )
 }
 export default Episodes
