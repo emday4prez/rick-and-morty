97aeb41406e472b9ba7e8e27b21e1acd465ca108
Emerson <emerson.day@gmail.com>
2023-06-01 16:04:24 -0700

    added styling

--- src/pages/episodes/index.tsx
+++ src/pages/episodes/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import s1 from '../../images/seasons/1.png'
 import s2 from '../../images/seasons/2.png'
 import s3 from '../../images/seasons/3.png'
@@ -23,12 +24,24 @@ const seasons = [
 function Episodes() {
     return (
         <div className="flex flex-col min-h-screen items-center">
-            <h1 className="text-6xl">episodes</h1>
-            <div>
+            <h1 className="text-6xl p-10 text-slate-700">episodes</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-5 lg:gap-8 xl:gap-10">
                 {seasons.map((season) => {
                     return (
-                        <div key={season.id}>
-                            <h2>Season {season.id}</h2>
+                        <div
+                            key={season.id}
+                            className="flex flex-col items-center justify-center bg-slate-900 rounded-xl shadow-xl hover:shadow-lg transition-shadow cursor-pointer"
+                        >
+                            <h2 className="text-xl font-bold text-slate-100 p-2">
+                                Season {season.id}
+                            </h2>
+                            <div>
+                                <Image
+                                    className="rounded-b-xl"
+                                    src={season.image}
+                                    alt={`season ${season.id} cover art`}
+                                ></Image>
+                            </div>
                         </div>
                     )
                 })}
