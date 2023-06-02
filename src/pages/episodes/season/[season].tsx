5ec6158da447543818fd59c68780aa853f459ed9
Emerson <emerson.day@gmail.com>
2023-06-01 17:23:49 -0700

    get serversideprops

--- src/pages/episodes/season/[season].tsx
+++ src/pages/episodes/season/[season].tsx
@@ -1,3 +1,32 @@
-export default function Season() {
-    return <div>Season</div>
+import { fetcher } from '../../../utils/fetch'
+export default function Season({ episodes, season }: any) {
+    return (
+        <>
+            {episodes && (
+                <div>
+                    <h2>Season {season}</h2>
+                    <div>
+                        {episodes.map((episode: any) => {
+                            return <div key={episode.id}>{episode.name}</div>
+                        })}
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
+
+export async function getServerSideProps({ params }: any) {
+    const { season } = params
+    console.log(season)
+    const seasonResponse = await fetcher(
+        `https://rickandmortyapi.com/api/episode/1,2,3,4,5,6,7,8,9,10,11`
+    )
+
+    return {
+        props: {
+            episodes: seasonResponse,
+            season,
+        },
+    }
 }
